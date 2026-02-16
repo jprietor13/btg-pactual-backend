4615188cf09897d150173289f60c38221c6d0dd6
Juan Prieto <jp1739@gmail.com>
2026-02-16 18:58:03 -0500

    feat: create modules and install dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { FundsModule } from './modules/funds/funds.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, FundsModule, TransactionsModule, AuthModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
