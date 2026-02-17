ea6690902c3ac9e4e3b2de8b81d4deb33adae858
Juan Prieto <jp1739@gmail.com>
2026-02-16 19:34:59 -0500

    feat: create initial seed for tests and subscribe DTO

---
+++ src/database/seeds/funds.seed.ts
@@ -0,0 +1,11 @@
+export const fundsSeed = [
+  {
+    name: 'FPV_BTG_PACTUAL_RECAUDADORA',
+    category: 'FPV',
+    minimumAmount: 75000,
+  },
+  { name: 'FPV_BTG_PACTUAL_ECOPETROL', category: 'FPV', minimumAmount: 125000 },
+  { name: 'DEUDAPRIVADA', category: 'FIC', minimumAmount: 50000 },
+  { name: 'FDO-ACCIONES', category: 'FIC', minimumAmount: 250000 },
+  { name: 'FPV_BTG_PACTUAL_DINAMICA', category: 'FPV', minimumAmount: 100000 },
+];
